b686fe5a2e93b33d77585bcc02324c9d0647ea6c
Gerd Aschemann <gerd@aschemann.net>
2017-08-01 19:30:38 +0200

    kontext-e/uneven-modules/#2 Right or Wrong?
    I would have expected an error here since there is no (enable)
    relationship between importer and manager.

--- src/main/java/de/kontext_e/demo/importer/Importer.java
+++ src/main/java/de/kontext_e/demo/importer/Importer.java
@@ -1,9 +1,11 @@
 package de.kontext_e.demo.importer;
 
 import de.kontext_e.demo.core.Core;
+import de.kontext_e.demo.manager.Manager;
 import de.kontext_e.demo.parser.Parser;
 
 public class Importer {
     private Parser parser;
     private Core core;
+    private Manager manager;
 }
